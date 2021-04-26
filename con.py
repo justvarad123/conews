73b9d1aac2410989402dc3a4d3349586ffcb3ee6
justvarad123 <33244550+justvarad123@users.noreply.github.com>
2021-04-26 12:27:45 +0530

    Update con.py

--- con.py
+++ con.py
@@ -26,9 +26,41 @@ def processRequest(req):
         for data in sp.find_all('h5'):
             d=data.text
             s.append(d)
-        cases="Confirmed"+":"+s[0]+'\n'+"Active"+":"+s[1]+'\n'+"Recoverd"+":"+s[2]+'\n'+"Deaths"+":"+s[3]
-    return {
-        "fulfillmentText": cases
-    	    }
+        cases="Confirmed"+":"+s[0]+'\n'+"Acive"+":"+s[1]+'\n'+"Recoverd"+":"+s[2]+'\n'+"Deaths"+":"+s[3]
+        return {
+            "fulfillmentText": cases
+         }
+    elif parameters.get("beds") :
+        url="http://117.204.250.138/Covid19mgt/CovidBedInfo.aspx"
+        ds=requests.get(url)
+        sp=bs4.BeautifulSoup(ds.text,'html.parser')
+        n=[]
+        ao=[]
+        co=[]
+        ib=[]
+        wb=[]
+        start = "\033[1m"
+        end = "\033[0;0m"
+        for data in sp.find_all('tr')[26:29]:
+            for d in data.find_all('td')[2]:
+                n.append(d)
+        for data in sp.find_all('tr')[26:29]:
+            for d in data.find_all('td')[4]:
+                ao.append(d)
+        for data in sp.find_all('tr')[26:29]:
+            for d in data.find_all('td')[5]:
+                co.append(d)
+        for data in sp.find_all('tr')[26:29]:
+            for d in data.find_all('td')[7]:
+                ib.append(d)
+        for data in sp.find_all('tr')[26:29]:
+            for d in data.find_all('td')[10]:
+                wb.append(d)
+        info=start+"1) "+n[0]+end+'\n'+"Administrative Officer :"+" "+ao[0]+'\n'+"Contact No :"+" "+co[0]+'\n'+"ICU Beds Available :"+" "+ib[0]+'\n'+"Ward Beds Available : "+" "+wb[0]+'\n'+'\n'+start+"2) "+n[1]+end+'\n'+"Administrative Officer :"+" "+ao[1]+'\n'+"Contact No :"+" "+co[1]+'\n'+"ICU Beds Available :"+" "+ib[1]+'\n'+"Ward Beds Available : "+" "+wb[1]+'\n'+'\n'+start+"3) "+n[2]+end+'\n'+"Administrative Officer :"+" "+ao[2]+'\n'+"Contact No :"+" "+co[2]+'\n'+"ICU Beds Available :"+" "+ib[2]+'\n'+"Ward Beds Available : "+" "+wb[2]
+        return {
+            "fulfillmentText": info
+          }
+
+    
 if __name__ == '__main__':
     app.run(debug = True) 
